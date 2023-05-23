234e65cd2b131d062260e35bcee10073aced1766
mathi <mathi@LAPTOP-OIRD9CHQ>
2023-05-23 16:11:56 +0200

    Ajoutez des fichiers projet.

---
+++ TestSQL/DBO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace TestSQL
+{
+    internal class DBO
+    {
+        private string m_URL;
+        private string m_dbName;
+        private string m_ID;
+        private string m_mdp;
+        private MySqlConnection m_connection;
+
+       
+        public string dbName { get { return m_dbName; } set { m_dbName = value; } }
+
+        public DBO(string URL, string ID, string mdp, string databaseName)
+        {
+            m_URL = URL;
+            m_ID = ID;
+            m_mdp = mdp;
+            m_dbName = databaseName;
+            connect();
+        }
+
+        public void connect()
+        {
+            string connStr = "server=" + m_URL + ";user=" + m_ID + ";database=" + m_dbName + ";password=" + m_mdp + ";";
+            m_connection = new MySqlConnection(connStr);
+            m_connection.Open();
+        }
+        public void reconnect()
+        {
+            close();
+            connect();
+        }
+        public void close()
+        {
+            m_connection.Close();
+        }
+
+        public MySqlDataReader query(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, m_connection);
+            using (MySqlDataReader rdr = cmd.ExecuteReader())
+            {
+                return rdr;
+            }
+        }
+    }
+}
---
+++ TestSQL/Program.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace TP8_winform
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Giau");
+
+            //CreateCommand("SELECT * FROM test;",GetConnectionString());
+            try
+            {
+                Mymethod();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.Read();
+        }
+        static void Mymethod()
+        {
+           
+            string connStr = "server=localhost;user=root;database=test;password=;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            string sql = "SELECT * FROM teyst";
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            using (MySqlDataReader rdr = cmd.ExecuteReader())
+            {
+                while (rdr.Read())
+                {
+                    Console.WriteLine("Donnée");
+                }
+            }
+            
+        }
+        //private static void CreateCommand(string queryString, string connectionString)
+        //{
+        //    using (SqlConnection connection = new SqlConnection(
+        //               connectionString))
+        //    {
+        //        SqlCommand command = new SqlCommand(queryString, connection);
+        //        command.Connection.Open();
+        //        command.ExecuteNonQuery();
+        //    }
+        //}
+        //static private string GetConnectionString()
+        //{
+        //    return "server=localhost;database=test;uid=root;pwd=;";
+        //}
+    }
+}
